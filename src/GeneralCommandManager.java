553b9042f8852fb48e21be701f4f9a46414f923c
Lolwel21 Camp <lolwel21camp@Lolwel21s-MacBook-Pro-3.local>
2019-06-14 17:35:41 -0400

    Added view permissions for instances

--- src/GeneralCommandManager.java
+++ src/GeneralCommandManager.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
 import java.util.List;
 
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Channel;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.managers.ChannelManager;
 import net.dv8tion.jda.core.requests.restaction.ChannelAction;
+import net.dv8tion.jda.core.requests.restaction.PermissionOverrideAction;
 
 // Handles commands from the General thread(s).
 public class GeneralCommandManager {
+  /*
+  Database db;
+  public GeneralCommandManager(Database db) {
+    this.db = db;
+  }
+  */
 
   // Process a command. Assume the command begins with the proper delimiter.
   public void processCommand(MessageReceivedEvent commandEvent) {
@@ -29,6 +38,7 @@ public class GeneralCommandManager {
     }
 
     if (command.equals("createchannel")) {
+      // Create a new channel
       String channelname = words[1];
       for (int i = 2; i < words.length; i++) {
         channelname = channelname + "-" + words[i];
@@ -39,36 +49,59 @@ public class GeneralCommandManager {
       List<Channel> channels = commandEvent.getGuild().getChannels();
 
       boolean nameused = false;
-
-      for (Channel c : channels) {
-        if (channelname.equalsIgnoreCase(c.getName())) {
-          nameused = true;
-          break;
+      if (channels.size() > 0) {
+        for (Channel c : channels) {
+          if (channelname.equalsIgnoreCase(c.getName())) {
+            nameused = true;
+            break;
+          }
         }
       }
-      ChannelManager channelmanager = null;
+      
       if (!nameused) {
         ChannelAction channelaction = commandEvent.getGuild().getController()
             .createTextChannel(channelname);
         channelaction.setTopic("Testing Channels");
         channelaction.setParent(commandEvent.getGuild().getCategoriesByName(parent, true).get(0));
-        //channelaction.addPermissionOverride(commandEvent.getGuild().getPublicRole(), 0, deny)
+        // Set public permissions (only members may view channel)
+        ArrayList<Permission> publicDeny = new ArrayList<Permission>();
+        publicDeny.addAll(Permission.getPermissions(Permission.ALL_TEXT_PERMISSIONS));
+        publicDeny.add(Permission.VIEW_CHANNEL);
+        channelaction.addPermissionOverride(commandEvent.getGuild().getPublicRole(),
+            new ArrayList<Permission>(), publicDeny);
+        // Set author permissions
+        ArrayList<Permission> authorAllow = new ArrayList<Permission>();
+        authorAllow.add(Permission.VIEW_CHANNEL);
+        authorAllow.add(Permission.MESSAGE_READ);
+        authorAllow.add(Permission.MESSAGE_WRITE);
+        channelaction.addPermissionOverride(commandEvent.getMember(), authorAllow,
+            new ArrayList<Permission>());
+
         channelaction.queue();
-        
+        channel.sendMessage("Created channel: " + channelname);
       }
       else {
         channel.sendMessage("name already used").queue();
       }
-      
-      for (Channel c : channels) {
-        if (c.getName().equals(channelname)) {
-          channelmanager = channels.get(0).getManager();
-        }
-      }
-      // TODO: Edit channel settings
-      if(channelmanager == null) {
-        System.out.println("Ugh. Couldn't find a siutable channel manager.");
-      }
     }
+    else if (command.contentEquals("join")) {
+      // Request to join an instance
+
+      // TODO: Check for permission to join instance
+
+      // Join the channel
+      // TODO: Search Database for channel id using name
+      /*
+      TextChannel tc = commandEvent.getGuild().getTextChannelById(id);
+      PermissionOverrideAction poa = tc.createPermissionOverride(commandEvent.getMember());
+      ArrayList<Permission> permissions = new ArrayList<Permission>();
+      permissions.add(Permission.MESSAGE_READ);
+      permissions.add(Permission.MESSAGE_WRITE);
+      permissions.add(Permission.VIEW_CHANNEL);
+      poa.setAllow(permissions);
+      poa.queue();
+      */
+    }
+
   }
 }
--- src/InstanceCommandManager.java
+++ src/InstanceCommandManager.java
@@ -1,40 +1,64 @@
+import java.util.ArrayList;
 import java.util.List;
+
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.requests.restaction.PermissionOverrideAction;
 
 // Handles commands from Instance threads.
 public class InstanceCommandManager {
-	
-	//Process a command. Assume the command begins with the proper delimiter.
-	public void processCommand(MessageReceivedEvent commandEvent) {
-		String[] words = commandEvent.getMessage().getContentStripped().split(" ");
-		// Determine the command.
-		String command = words[0].substring(1);
-		
-		
-		if(command.equals("invite")) {
-			// Invite command.
-			List<Member> invited = commandEvent.getMessage().getMentionedMembers();
-			String senderName = commandEvent.getAuthor().getName();
-			String channelName = commandEvent.getChannel().getName();
-			for(Member m : invited) {
-				//TODO: Add the invite to the SQL database.
-				sendPrivateMessage(m.getUser(), senderName + " invited you to join "+channelName);
-			}
-		} else if(command.equals("leave")) {
-			// Leave the instance
-		}
-		
-		
-	}
-	
-	public void sendPrivateMessage(User user, String content) {
-        // openPrivateChannel provides a RestAction<PrivateChannel>
-        // which means it supplies you with the resulting channel
-        user.openPrivateChannel().queue((channel) ->
-        {
-            channel.sendMessage(content).queue();
-        });
+  /*
+  Database db;
+  public GeneralCommandManager(Database db) {
+    this.db = db;
+  }
+  */
+  
+  // Process a command. Assume the command begins with the proper delimiter.
+  public void processCommand(MessageReceivedEvent commandEvent) {
+    String[] words = commandEvent.getMessage().getContentStripped().split(" ");
+    // Determine the command.
+    String command = words[0].substring(1);
+
+    if (command.equals("invite")) {
+      // Invite command.
+      List<Member> invited = commandEvent.getMessage().getMentionedMembers();
+      String senderName = commandEvent.getAuthor().getName();
+      String channelName = commandEvent.getChannel().getName();
+      for (Member m : invited) {
+        // TODO: Add the invite to the SQL database.
+        sendPrivateMessage(m.getUser(), senderName + " invited you to join " + channelName);
+      }
     }
+    else if (command.equals("leave")) {
+      // Leave the instance
+      if (commandEvent.getTextChannel().getMembers().size() == 1) {
+        // This is the last member. Delete the channel.
+        commandEvent.getTextChannel().delete();
+      } else {
+        // Not the last member. Simply remove permissions.
+        ArrayList<Permission> permissions = new ArrayList<Permission>();
+        permissions.add(Permission.MESSAGE_READ);
+        permissions.add(Permission.MESSAGE_WRITE);
+        permissions.add(Permission.VIEW_CHANNEL);
+        PermissionOverrideAction override = commandEvent.getTextChannel()
+            .createPermissionOverride(commandEvent.getMember());
+        override.setDeny(permissions);
+        override.queue();
+      }
+    }
+    else if (command.equals("start")) {
+      //TODO: Implement database functions and game functions
+    }
+  }
+
+  public void sendPrivateMessage(User user, String content) {
+    // openPrivateChannel provides a RestAction<PrivateChannel>
+    // which means it supplies you with the resulting channel
+    user.openPrivateChannel().queue((channel) -> {
+      channel.sendMessage(content).queue();
+    });
+  }
 }
